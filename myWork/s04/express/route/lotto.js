4dd406d3b17c59470fee2e4345b12bd1f98bcc4d
OskarForssman <Oskar.forssman@hotmail.com>
2020-02-12 15:16:14 +0100

    Hej

---
+++ myWork/s04/express/route/lotto.js
@@ -0,0 +1,23 @@
+/**
+ * Route for today.
+ */
+"use strict";
+
+var express = require("express");
+var router  = express.Router();
+
+router.get("/", (req, res) => {
+
+ let data={};
+data.lotonumb=[]
+for (var i=0;i<7;i++){
+    data.lotonumb[i]=Math.floor(Math.random() * 35)+1; 
+    
+} 
+
+
+res.render("lotto", data);
+    
+});
+
+module.exports = router;
